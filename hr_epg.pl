1807a19391b12b20d0b587184648d4b666dcbd93
Mark <mark@localhost.localdomain>
2019-07-01 16:11:44 +1000

    Small updates

--- hr_epg.pl
+++ hr_epg.pl
@@ -6,8 +6,13 @@ use Time::Local;
 my %map = (
     '&' => 'and',
 );
-my $tz = "+1000";
 my $chars = join '', keys %map;
+
+my $tz; # = "+1000";
+my @lt = localtime(12*60*60);
+my @gt = gmtime(12*60*60);
+$tz = $lt[2] - $gt[2];
+$tz = sprintf("%+03d00", $tz); 
 
 my $curl = "/usr/bin/curl";
 my $data = `$curl -s -L http://ipv4-api.hdhomerun.com/discover`;
@@ -42,7 +47,7 @@ foreach my $items (@$guidedata) {
   };
   print "\t<channel id=\"".$channel.".".$items->{GuideName}."\">\n";
   printf "\t\t<display-name>".$channelname."</display-name>\n";
-  printf "\t\t<icon src=\"".$items->{ImageURL}."\" />\n";
+  printf "\t\t<icon src=\"".$items->{ImageURL}."\" />\n" if (defined($items->{ImageURL}));
   print "\t</channel>\n";
  
 }
@@ -71,6 +76,7 @@ sub printprogrammexml {
         my $endtime = $items->{EndTime};
         my $title = $items->{Title};
         my $movie = 0;
+        my $originalairdate = "";
         my ($ssec,$smin,$shour,$smday,$smon,$syear,$swday,$syday,$sisdst) = localtime($starttime);
         my ($esec,$emin,$ehour,$emday,$emon,$eyear,$ewday,$eyday,$eisdst) = localtime($endtime);
         my $startdate=sprintf("%0.4d%0.2d%0.2d%0.2d%0.2d%0.2d",($syear+1900),$smon+1,$smday,$shour,$smin,$ssec);
@@ -95,11 +101,13 @@ sub printprogrammexml {
                }
                print "\t\t<category lang=\"en\">$category</category>\n";
            }
-        }                
+        }
+        print "\t\t<icon src=\"$items->{ImageURL}\" />\n" if (defined($items->{ImageURL}));                
         if (defined($items->{EpisodeNumber})) {
            my $series = 0;
            my $episode = 0;
            if ($items->{EpisodeNumber} =~ /^S/) {
+              print "\t\t<episode-num system=\"SxxExx\">$items->{EpisodeNumber}</episode-num>\n" if (defined($items->{EpisodeNumber}));
               ($series, $episode) = $items->{EpisodeNumber} =~ /S(.+)E(.+)/;
               $series--;
               $episode--;
@@ -118,16 +126,23 @@ sub printprogrammexml {
            print "\t\t<episode-num system=\"xmltv_ns\">$series.$episode.</episode-num>\n" if (defined($items->{EpisodeNumber}));
             
         }
-        if ((defined($items->{SeriesID})) and (!defined($items->{OriginalAirdate})) and !($movie)) {
-            my $startdate=sprintf("%0.4d-%0.2d-%0.2d %0.2d:%0.2d:%0.2d",($syear+1900),$smon+1,$smday,$shour,$smin,$ssec);        
+#        if ((defined($items->{SeriesID})) and (!defined($items->{OriginalAirdate})) and !($movie)) {
+         if ((!defined($items->{EpisodeNumber})) and (!defined($items->{OriginalAirdate})) and !($movie)) {
+            my $startdate=sprintf("%0.4d-%0.2d-%0.2d %0.2d:%0.2d:%0.2d",($syear+1900),$smon+1,$smday,$shour,$smin,$ssec);
+            my $tmpseries = sprintf("S%0.4dE%0.2d%0.2d%0.2d%0.2d%0.2d",($syear+1900),($smon+1),$smday,$shour,$smin,$ssec);                    
             print "\t\t<episode-num system=\"original-air-date\">$startdate</episode-num>\n";
+            print "\t\t<episode-num system=\"SxxExx\">$tmpseries</episode-num>\n";
         }
         if (defined($items->{OriginalAirdate})) {
            my ($oadsec,$oadmin,$oadhour,$oadmday,$oadmon,$oadyear,$oadwday,$oadyday,$oadisdst) = localtime($items->{OriginalAirdate});
-           my $originalairdate = sprintf("%0.4d-%0.2d-%0.2d %0.2d:%0.2d:%0.2d",($oadyear+1900),$oadmon+1,$oadmday,$oadhour,$oadmin,$oadsec);                    
-           print "\t\t<episode-num system=\"original-air-date\">$originalairdate</episode-num>\n";
+           $originalairdate = sprintf("%0.4d-%0.2d-%0.2d %0.2d:%0.2d:%0.2d",($oadyear+1900),$oadmon+1,$oadmday,$oadhour,$oadmin,$oadsec);                    
+           print "\t\t<episode-num system=\"original-air-date\">$originalairdate</episode-num>\n";           
         }
-        print "\t\t<icon src=\"$items->{ImageURL}\" />\n" if (defined($items->{ImageURL}));
+        
+        if ($originalairdate ne "") {
+            print "\t\t<previously-shown start=\"$originalairdate\" />\n";
+        }
+        else { print "\t\t<previously-shown />\n";}
         print "\t</programme>\n";
     }
 }
